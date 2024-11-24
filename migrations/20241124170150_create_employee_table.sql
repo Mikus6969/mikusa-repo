1ce5f89d55b9b055be4eb23678e26249eaa96a87
Mikus6969 <mikus.ozols2004@gmail.com>
2024-11-24 17:19:48 +0200

    w

---
+++ migrations/20241124170150_create_employee_table.sql
@@ -0,0 +1,8 @@
+
+            CREATE TABLE IF NOT EXISTS Employees (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                surname TEXT NOT NULL,
+                personal_id INTEGER
+            );
+        
